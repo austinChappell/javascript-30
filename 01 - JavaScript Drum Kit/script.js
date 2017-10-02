0f00081db18d62fd4bdb707ab9787d38a975a6dc
Austin Chappell <austin@cratebind.com>
2017-10-02 15:57:36 -0500

    day 1 complete

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,21 +1,27 @@
-const sounds = document.querySelectorAll('audio'),
-      clap = sounds[0],
-      hihat = sounds[1],
-      kick = sounds[2],
-      openhat = sounds[3],
-      boom = sounds[4],
-      ride = sounds[5],
-      snare = sounds[6],
-      tom = sounds[7],
-      tink = sounds[8];
+const sounds = document.querySelectorAll('audio');
+const keys = document.querySelectorAll('.key');
 
 console.log(sounds);
+console.log(keys);
 
-document.addEventListener('keyup', (evt) => {
+document.addEventListener('keydown', (evt) => {
   sounds.forEach((sound) => {
     if (sound.dataset.key == evt.keyCode) {
+      if (!sound.paused) {
+        console.log('paused');
+        sound.pause();
+        sound.currentTime = 0;
+      }
       sound.play();
       sound.classList.add('playing');
+    }
+  })
+  keys.forEach((key) => {
+    if (key.dataset.key == evt.keyCode) {
+      key.classList.add('playing');
+      setTimeout(() => {
+        key.classList.remove('playing');
+      }, 100);
     }
   })
 });
