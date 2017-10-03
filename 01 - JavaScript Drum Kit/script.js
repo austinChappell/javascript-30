39eff08898aeadb2224a98e09d2606a8b553a351
Austin Chappell <austin@cratebind.com>
2017-10-03 08:10:29 -0500

    add closed hi hat nuance

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,10 +1,12 @@
 const sounds = document.querySelectorAll('audio');
 const keys = document.querySelectorAll('.key');
 
-console.log(sounds);
-console.log(keys);
-
 document.addEventListener('keydown', (evt) => {
+  // This stops an open hi-hat sound if a close hi-hat sound is trigerred
+  if (evt.keyCode == sounds[1].dataset.key && !sounds[3].paused) {
+    sounds[3].pause();
+    sounds[3].currentTime = 0;
+  }
   sounds.forEach((sound) => {
     if (sound.dataset.key == evt.keyCode) {
       if (!sound.paused) {
@@ -14,14 +16,14 @@ document.addEventListener('keydown', (evt) => {
       }
       sound.play();
       sound.classList.add('playing');
-    }
-  })
-  keys.forEach((key) => {
-    if (key.dataset.key == evt.keyCode) {
-      key.classList.add('playing');
-      setTimeout(() => {
-        key.classList.remove('playing');
-      }, 100);
+      keys.forEach((key) => {
+        if (key.dataset.key == evt.keyCode) {
+          key.classList.add('playing');
+          setTimeout(() => {
+            key.classList.remove('playing');
+          }, 100);
+        }
+      })
     }
   })
 });
